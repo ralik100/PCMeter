0f4adf28bf9a204f47aeb246de16bb9480721dab
ralik100 <sbagniewski051@gmail.com>
2025-11-03 21:43:09 +0100

    v.2.0.9 - log_parser, added filtering by timestamp

--- src/log_parser.py
+++ src/log_parser.py
@@ -1,15 +1,34 @@
 import re
 
-
 def log_parser(log_file_path):
+    result=[]
     with open(log_file_path,"r") as f:
         for line in f:
             line.strip()
+            log_timestamp=re.search(r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+)", line)
             res=re.search(r"CPU usage:\s*([0-9.]+)%", line)
-            if res:
-                usage=float(res.group(1))
-                print(usage)
+            if log_timestamp:
+                words=line.split(" ")
+                timestamp=str(log_timestamp.group(1))
+                metric=words[3]
+                usage=get_usage(line,metric)
+                print(f"{metric}, {usage}")
 
-
+def get_usage(line, metric):
+    match metric:
+        case "CPU":
+            res=re.search(r"CPU usage:\s*([0-9.]+)%", line)
+            return float(res.group(1))
+        case "GPU":
+            res=re.search(r"GPU usage:\s*([0-9.]+)%", line)
+            return float(res.group(1))
+        case "Disc":
+            res=re.search(r"Disc usage:\s*([0-9.]+)%", line)
+            return float(res.group(1))
+        case "RAM":
+            res=re.search(r"RAM usage:\s*([0-9.]+)%", line)
+            return float(res.group(1))
+        case _:
+            pass
 
 log_parser("log.txt")
